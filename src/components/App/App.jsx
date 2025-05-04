e6aef6d16b3559b740bfae3b79d37b9d9ffb21ae
Lina Parashchenko <blond2109@gmail.com>
2025-05-04 22:52:27 +0300

    app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,42 @@
 import css from "./App.module.css";
+import { useState } from "react";
 
-import userData from "../../UserData.json";
-import friends from "../../friends.json";
-import transactions from "../../transactions.json";
-
-import Profile from "../Profile/Profile";
-import FriendList from "../FriendList/FriendList";
-import TransactionHistory from "../TransactionHistory/TransactionHistory";
-
-const { username, tag, location, avatar, stats } = userData;
+import ContactForm from "../ContactForm/ContactForm";
+import ContactList from "../ContactList/ContactList";
+import SearchBox from "../SearchBox/SearchBox";
 
 export default function App() {
+  const [contacts, setContacts] = useState([
+    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  ]);
+
+  const [filter, serFilter] = useState("");
+
+  const addContact = (newContact) => {
+    setContacts((prevContact) => {
+      return [...prevContact, newContact];
+    });
+  };
+
+  const deleteContact = (contactId) => {
+    setContacts((prevContact) => {
+      return prevContacts.filter((contact) => contact.id !== contactId);
+    });
+  };
+
+  const filterContacts = contacts.filtred((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div className={css.container}>
-      <Profile
-        name={username}
-        tag={tag}
-        location={location}
-        image={avatar}
-        stats={stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onAdd={addContact} />
+      <SearchBox value={filter} onFilter={setFilter} />
+      <ContactList contactsList={filterContacts} onDelete={deleteContact} />
     </div>
   );
 }
